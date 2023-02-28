20e79bf169e7897910d82cfa5bd58f4eb96fe4c2
Fimilak <filip.milak@mercedes-benz.com>
2023-02-28 21:27:04 +0100

    update download.py

--- download.py
+++ download.py
@@ -1,4 +1,8 @@
 import tensorflow_datasets as tfds
 import tensorflow as tf
 
-ds = tfds.load('wider_face', split='train', shuffle_files=True)
+dl_config = tfds.download.DownloadConfig(verify_ssl = False)
+
+tfds.load('wider_face', split="train", shuffle_files=True, with_info=True, download_and_prepare_kwargs={
+      'download_config': dl_config,
+})
